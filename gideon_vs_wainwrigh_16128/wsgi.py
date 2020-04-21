b39ebc929db2afaffa3c97d828c4fd0ec62ff021
Crowdbotics <team@crowdbotics.com>
2020-04-21 22:46:23 +0000

    Initial commit.

---
+++ gideon_vs_wainwrigh_16128/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gideon_vs_wainwrigh_16128 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gideon_vs_wainwrigh_16128.settings')
+
+application = get_wsgi_application()
